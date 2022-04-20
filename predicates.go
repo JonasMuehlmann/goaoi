dab6d65939e983fdcfa210df4870e6afab532f49
jonas <jonasmuehlmann@protonmail.com>
2022-04-20 15:16:15 +0200

    feat(predicates): implement predicate builders.
    All predicates `XXX` have a new variant `XXXPartial` which partially
    specializes the function.
    
    closes: #5

--- predicates.go
+++ predicates.go
@@ -6,22 +6,46 @@ func AreEqual[T comparable](a T, b T) bool {
 	return a == b
 }
 
+func AreEqualPartial[T comparable](fixedComparee T) func(T) bool {
+	return func(comparee T) bool { return comparee == fixedComparee }
+}
+
 func AreNotEqual[T comparable](a T, b T) bool {
 	return a != b
+}
+
+func AreNotEqualPartial[T comparable](fixedComparee T) func(T) bool {
+	return func(comparee T) bool { return comparee != fixedComparee }
 }
 
 func IsLessThan[T constraints.Ordered](a T, b T) bool {
 	return a < b
 }
 
+func IsLessThanPartial[T constraints.Ordered](fixedComparee T) func(T) bool {
+	return func(comparee T) bool { return comparee < fixedComparee }
+}
+
 func IsLessThanEqual[T constraints.Ordered](a T, b T) bool {
 	return a <= b
+}
+
+func IsLessThanEqualPartial[T constraints.Ordered](fixedComparee T) func(T) bool {
+	return func(comparee T) bool { return comparee <= fixedComparee }
 }
 
 func IsGreaterThan[T constraints.Ordered](a T, b T) bool {
 	return a > b
 }
 
+func IsGreaterThanPartial[T constraints.Ordered](fixedComparee T) func(T) bool {
+	return func(comparee T) bool { return comparee < fixedComparee }
+}
+
 func IsGreaterThanEqual[T constraints.Ordered](a T, b T) bool {
 	return a >= b
+}
+
+func IsGreaterThanEqualPartial[T constraints.Ordered](fixedComparee T) func(T) bool {
+	return func(comparee T) bool { return comparee >= fixedComparee }
 }
